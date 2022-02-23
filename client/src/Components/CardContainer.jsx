c2795d9ac553c8f5501edf7ae68fa10c9269c5a4
Shubh-am-15 <shubhamsingh15052001@gmail.com>
2022-02-23 11:38:47 +0530

    modals and cards fixed

--- client/src/Components/CardContainer.jsx
+++ client/src/Components/CardContainer.jsx
@@ -59,18 +59,86 @@ function CardContainer() {
           <div className="col-lg-3 col-md-4 col-sm-12 m-3 ">
             <Card.Body className="card">
               <Card.Title className="cardTitle">
-                <strong>Card Title</strong>
+                <strong>{FilledForm.type}</strong>
               </Card.Title>
               <Card.Text className="cardDescription">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                <div>Sector: {FilledForm.company_overview.sector}</div>
+                <div>Designation: {FilledForm.job_detail.designation}</div>
               </Card.Text>
-              <Button className="cardButton" variant="outline-light">
-                Edit
+              <Button
+                className="cardButton"
+                variant="outline-light"
+                onClick={handleShow}
+              >
+                View
               </Button>
+
+              <Modal size="lg" show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>
+                    {FilledForm.type === "INF" ? (
+                      <div>Internship Notification Form</div>
+                    ) : (
+                      <div>Job Notification Form</div>
+                    )}
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <FormModal unique_id={FilledForm.unique_id} />
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button>
+                </Modal.Footer>
+              </Modal>
             </Card.Body>
           </div>
           <div className="col-lg-3 col-md-4 col-sm-12 m-3 ">
+            <Card.Body className="card">
+              <Card.Title className="cardTitle">
+                <strong>{FilledForm.type}</strong>
+              </Card.Title>
+              <Card.Text className="cardDescription">
+                <div>Sector: {FilledForm.company_overview.sector}</div>
+                <div>Designation: {FilledForm.job_detail.designation}</div>
+              </Card.Text>
+              <Button
+                className="cardButton"
+                variant="outline-light"
+                onClick={handleShow}
+              >
+                View
+              </Button>
+
+              <Modal size="lg" show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>
+                    {FilledForm.type === "INF" ? (
+                      <div>Internship Notification Form</div>
+                    ) : (
+                      <div>Job Notification Form</div>
+                    )}
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <FormModal unique_id={FilledForm.unique_id} />
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </Card.Body>
+          </div>
+          {/* <div className="col-lg-3 col-md-4 col-sm-12 m-3 ">
             <Card.Body className="card">
               <Card.Title className="cardTitle">
                 <strong>Card Title</strong>
@@ -125,7 +193,21 @@ function CardContainer() {
                 Edit
               </Button>
             </Card.Body>
-          </div>
+          </div> */}
+          {/* <div className="col-lg-3 col-md-4 col-sm-12 m-3 ">
+            <Card.Body className="card">
+              <Card.Title className="cardTitle">
+                <strong>Card Title</strong>
+              </Card.Title>
+              <Card.Text className="cardDescription">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button className="cardButton" variant="outline-light">
+                Edit
+              </Button>
+            </Card.Body>
+          </div> */}
         </div>
       </div>
     </>
--- client/src/Components/FormModal.js
+++ client/src/Components/FormModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FilledForm } from "../constants/FilledForm";
+import "../Styles/FormModal.css"
 
 export default function FormModal(props) {
   // get filled form from unique id
@@ -7,86 +8,88 @@ export default function FormModal(props) {
     <div>
       <div>
         <div className="categoryName">Company Overview</div>
-        <div className="headingdiv">
-          <div className="modalHeading">Name of Company:</div>
-          <div className="headingValue">{FilledForm.company_overview.name}</div>
+        <div className="headingdiv row">
+          <div className="modalHeading col-sm-6">Name of Company:</div>
+          <div className="headingValue col-sm-6">{FilledForm.company_overview.name}</div>
         </div>
         <div className="headingdiv">
-          <div className="modalHeading">Sector:</div>
-          <div className="headingValue">
+          <div className="modalHeading col-sm-6">Sector: </div>
+          <div className="headingValue col-sm-6">
             {FilledForm.company_overview.sector}
           </div>
         </div>
         <div className="headingdiv">
-          <div className="modalHeading">Website:</div>
-          <div className="headingValue">
+          <div className="modalHeading col-sm-6">Website: </div>
+          <div className="headingValue col-sm-6">
             {FilledForm.company_overview.website}
           </div>
         </div>
       </div>
+      <hr />
       <div>
         <div className="categoryName">Job Details</div>
         <div className="headingdiv">
-          <div className="modalHeading">Designation:</div>
-          <div className="headingValue">
+          <div className="modalHeading col-sm-6">Designation: </div>
+          <div className="headingValue col-sm-6">
             {FilledForm.job_detail.designation}
           </div>
         </div>
 
         <div className="headingdiv">
-          <div className="modalHeading">Place of Posting:</div>
-          <div className="headingValue">
+          <div className="modalHeading col-sm-6">Place of Posting: </div>
+          <div className="headingValue col-sm-6">
             {FilledForm.job_detail.place_of_posting}
           </div>
         </div>
 
         <div className="headingdiv">
-          <div className="modalHeading">Description:</div>
-          <div className="headingValue">
+          <div className="modalHeading col-sm-6">Description: </div>
+          <div className="headingValue col-sm-6">
             {FilledForm.job_detail.description}
           </div>
         </div>
         <div className="headingdiv">
-          <div className="modalHeading">Duration:</div>
-          <div className="headingValue">{FilledForm.job_detail.duration}</div>
+          <div className="modalHeading col-sm-6">Duration : </div>
+          <div className="headingValue col-sm-6"> {FilledForm.job_detail.duration}</div>
         </div>
         {FilledForm.type === "INF" ? (
           <div className="headingdiv">
-            <div className="modalHeading">Mode of Internship:</div>
-            <div className="headingValue">{FilledForm.job_detail.mode}</div>
+            <div className="modalHeading col-sm-6">Mode of Internship: </div>
+            <div className="headingValue col-sm-6">{FilledForm.job_detail.mode}</div>
           </div>
         ) : (
           <div></div>
         )}
       </div>
+      <hr />
       <div>
-        <div className="categoryName">Contact Details:</div>
+        <div className="categoryName">Contact Details: </div>
         <div>
-          <div>Primary Contact detail</div>
+          <div className="lead">Primary Contact detail</div>
           <div>
             <div className="headingdiv">
-              <div className="modalHeading">Name:</div>
-              <div className="headingValue">
+              <div className="modalHeading col-sm-6">Name: </div>
+              <div className="headingValue col-sm-6">
                 {FilledForm.contact_detail[0].name}
               </div>
             </div>
 
             <div className="headingdiv">
-              <div className="modalHeading">Designation:</div>
-              <div className="headingValue">
+              <div className="modalHeading col-sm-6">Designation: </div>
+              <div className="headingValue col-sm-6">
                 {FilledForm.contact_detail[0].designation}
               </div>
             </div>
 
             <div className="headingdiv">
-              <div className="modalHeading">Email:</div>
-              <div className="headingValue">
+              <div className="modalHeading col-sm-6">Email: </div>
+              <div className="headingValue col-sm-6">
                 {FilledForm.contact_detail[0].email}
               </div>
             </div>
             <div className="headingdiv">
-              <div className="modalHeading">Mobile:</div>
-              <div className="headingValue">
+              <div className="modalHeading col-sm-6">Mobile: </div>
+              <div className="headingValue col-sm-6">
                 {FilledForm.contact_detail[0].mobile}
               </div>
             </div>
@@ -94,31 +97,31 @@ export default function FormModal(props) {
         </div>
         {FilledForm.contact_detail.length === 2 ? (
           <div>
-            <div>Secondary Contact detail</div>
+            <div className="lead">Secondary Contact detail</div>
             <div>
               <div className="headingdiv">
-                <div className="modalHeading">Name:</div>
-                <div className="headingValue">
+                <div className="modalHeading col-sm-6">Name: </div>
+                <div className="headingValue col-sm-6">
                   {FilledForm.contact_detail[1].name}
                 </div>
               </div>
 
               <div className="headingdiv">
-                <div className="modalHeading">Designation:</div>
-                <div className="headingValue">
+                <div className="modalHeading col-sm-6">Designation : </div>
+                <div className="headingValue col-sm-6">
                   {FilledForm.contact_detail[1].designation}
                 </div>
               </div>
 
               <div className="headingdiv">
-                <div className="modalHeading">Email:</div>
-                <div className="headingValue">
+                <div className="modalHeading col-sm-6">Email: </div>
+                <div className="headingValue col-sm-6">
                   {FilledForm.contact_detail[1].email}
                 </div>
               </div>
               <div className="headingdiv">
-                <div className="modalHeading">Mobile:</div>
-                <div className="headingValue">
+                <div className="modalHeading col-sm-6">Mobile: </div>
+                <div className="headingValue col-sm-6">
                   {FilledForm.contact_detail[1].mobile}
                 </div>
               </div>
@@ -128,6 +131,7 @@ export default function FormModal(props) {
           <div></div>
         )}
       </div>
+      <hr />
       <div>
         <div className="categoryName">Eligible Courses and Discipline</div>
         <div>
@@ -189,6 +193,7 @@ export default function FormModal(props) {
           </div>
         </div>
       </div>
+      <hr />
       <div>
         <div className="categoryName">Skills Requirement:</div>
         <div>
@@ -197,52 +202,54 @@ export default function FormModal(props) {
           })}
         </div>
       </div>
+      <hr />
       <div>
         <div className="categoryName">Selection Procedure</div>
         <div>
           <div className="headingdiv">
-            <div className="modalHeading">Resume Shortlisting :</div>
-            <div className="headingValue">
+            <div className="modalHeading col-sm-6">Resume Shortlisting : </div>
+            <div className="headingValue col-sm-6">
               {FilledForm.selection_pr.resume_short_listing}
             </div>
           </div>
 
           <div className="headingdiv">
-            <div className="modalHeading">Type of Test :</div>
-            <div className="headingValue">
+            <div className="modalHeading col-sm-6">Type of Test : </div>
+            <div className="headingValue col-sm-6">
               {FilledForm.selection_pr.type_of_test}
             </div>
           </div>
 
           <div className="headingdiv">
-            <div className="modalHeading">Other Qualification Rounds :</div>
+            <div className="modalHeading col-sm-6">Other Qualification Rounds : </div>
             {FilledForm.selection_pr.other_round.map((round) => {
-              <div className="headingValue">{round}</div>;
+              <div className="headingValue col-sm-6">{round}</div>;
             })}
           </div>
           <div className="headingdiv">
-            <div className="modalHeading">Total Number of rounds:</div>
-            <div className="headingValue">
+            <div className="modalHeading col-sm-6">Total Number of rounds: </div>
+            <div className="headingValue col-sm-6">
               {FilledForm.selection_pr.total_rounds}
             </div>
           </div>
           <div className="headingdiv">
-            <div className="modalHeading">
+            <div className="modalHeading col-sm-6">
               Number of offers available for IIT(ISM) students (Range would be
               sufficient)
             </div>
-            <div className="headingValue">
+            <div className="headingValue col-sm-6">
               {FilledForm.selection_pr.no_of_offers}
             </div>
           </div>
           <div className="headingdiv">
-            <div className="modalHeading">Eligibility Criteria (if any)</div>
-            <div className="headingValue">
+            <div className="modalHeading col-sm-6">Eligibility Criteria (if any)</div>
+            <div className="headingValue col-sm-6">
               {FilledForm.selection_pr.eligible_criteria}
             </div>
           </div>
         </div>
       </div>
+      <hr />
     </div>
   );
 }
