97719146047b3c214036efed5e5d090e65c47e14
krittikabarnwal <krittikabarnwal2014@gmail.com>
2022-02-28 15:22:34 +0530

    changes

--- client/src/Components/CardContainer.jsx
+++ client/src/Components/CardContainer.jsx
@@ -31,29 +31,6 @@ function CardContainer() {
               >
                 View
               </Button>
-
-              <Modal size="lg" show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>
-                    {FilledForm.type === "INF" ? (
-                      <div>Internship Notification Form</div>
-                    ) : (
-                      <div>Job Notification Form</div>
-                    )}
-                  </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <FormModal unique_id={FilledForm.unique_id} />
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-                  <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                  </Button>
-                </Modal.Footer>
-              </Modal>
             </Card.Body>
           </div>
           <div className="col-lg-3 col-md-4 col-sm-12 m-3 ">
@@ -72,29 +49,6 @@ function CardContainer() {
               >
                 View
               </Button>
-
-              <Modal size="lg" show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>
-                    {FilledForm.type === "INF" ? (
-                      <div>Internship Notification Form</div>
-                    ) : (
-                      <div>Job Notification Form</div>
-                    )}
-                  </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <FormModal unique_id={FilledForm.unique_id} />
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-                  <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                  </Button>
-                </Modal.Footer>
-              </Modal>
             </Card.Body>
           </div>
           <div className="col-lg-3 col-md-4 col-sm-12 m-3 ">
@@ -113,102 +67,31 @@ function CardContainer() {
               >
                 View
               </Button>
-
-              <Modal size="lg" show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>
-                    {FilledForm.type === "INF" ? (
-                      <div>Internship Notification Form</div>
-                    ) : (
-                      <div>Job Notification Form</div>
-                    )}
-                  </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  <FormModal unique_id={FilledForm.unique_id} />
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-                  <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                  </Button>
-                </Modal.Footer>
-              </Modal>
             </Card.Body>
           </div>
-          {/* <div className="col-lg-3 col-md-4 col-sm-12 m-3 ">
-            <Card.Body className="card">
-              <Card.Title className="cardTitle">
-                <strong>Card Title</strong>
-              </Card.Title>
-              <Card.Text className="cardDescription">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button className="cardButton" variant="outline-light">
-                Edit
-              </Button>
-            </Card.Body>
-          </div>
-          <div className="col-lg-3 col-md-4 col-sm-12 m-3 ">
-            <Card.Body className="card">
-              <Card.Title className="cardTitle">
-                <strong>Card Title</strong>
-              </Card.Title>
-              <Card.Text className="cardDescription">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button className="cardButton" variant="outline-light">
-                Edit
-              </Button>
-            </Card.Body>
-          </div>
-          <div className="col-lg-3 col-md-4 col-sm-12 m-3 ">
-            <Card.Body className="card">
-              <Card.Title className="cardTitle">
-                <strong>Card Title</strong>
-              </Card.Title>
-              <Card.Text className="cardDescription">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button className="cardButton" variant="outline-light">
-                Edit
-              </Button>
-            </Card.Body>
-          </div>
-          <div className="col-lg-3 col-md-4 col-sm-12 m-3 ">
-            <Card.Body className="card">
-              <Card.Title className="cardTitle">
-                <strong>Card Title</strong>
-              </Card.Title>
-              <Card.Text className="cardDescription">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button className="cardButton" variant="outline-light">
-                Edit
-              </Button>
-            </Card.Body>
-          </div> */}
-          {/* <div className="col-lg-3 col-md-4 col-sm-12 m-3 ">
-            <Card.Body className="card">
-              <Card.Title className="cardTitle">
-                <strong>Card Title</strong>
-              </Card.Title>
-              <Card.Text className="cardDescription">
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Button className="cardButton" variant="outline-light">
-                Edit
-              </Button>
-            </Card.Body>
-          </div> */}
         </div>
+        <Modal size="lg" show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>
+              {FilledForm.type === "INF" ? (
+                <div>Internship Notification Form</div>
+              ) : (
+                <div>Job Notification Form</div>
+              )}
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <FormModal unique_id={FilledForm.unique_id} />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </>
   );
--- client/src/Components/FormModal.js
+++ client/src/Components/FormModal.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 // import { props } from "../constants/props";
-import "../Styles/FormModal.css"
+import "../Styles/FormModal.css";
+import { FilledForm } from "../constants/FilledForm";
 
 export default function FormModal(props) {
   // get filled form from unique id
@@ -10,18 +11,20 @@ export default function FormModal(props) {
         <div className="categoryName">Company Overview</div>
         <div className="headingdiv row">
           <div className="modalHeading col-sm-6">Name of Company:</div>
-          <div className="headingValue col-sm-6">{props.company_overview.name}</div>
+          <div className="headingValue col-sm-6">
+            {FilledForm.company_overview.name}
+          </div>
         </div>
         <div className="headingdiv">
           <div className="modalHeading col-sm-6">Sector: </div>
           <div className="headingValue col-sm-6">
-            {props.company_overview.sector}
+            {FilledForm.company_overview.sector}
           </div>
         </div>
         <div className="headingdiv">
           <div className="modalHeading col-sm-6">Website: </div>
           <div className="headingValue col-sm-6">
-            {props.company_overview.website}
+            {FilledForm.company_overview.website}
           </div>
         </div>
       </div>
@@ -31,31 +34,36 @@ export default function FormModal(props) {
         <div className="headingdiv">
           <div className="modalHeading col-sm-6">Designation: </div>
           <div className="headingValue col-sm-6">
-            {props.job_detail.designation}
+            {FilledForm.job_detail.designation}
           </div>
         </div>
 
         <div className="headingdiv">
           <div className="modalHeading col-sm-6">Place of Posting: </div>
           <div className="headingValue col-sm-6">
-            {props.job_detail.place_of_posting}
+            {FilledForm.job_detail.place_of_posting}
           </div>
         </div>
 
         <div className="headingdiv">
           <div className="modalHeading col-sm-6">Description: </div>
           <div className="headingValue col-sm-6">
-            {props.job_detail.description}
+            {FilledForm.job_detail.description}
           </div>
         </div>
         <div className="headingdiv">
           <div className="modalHeading col-sm-6">Duration : </div>
-          <div className="headingValue col-sm-6"> {props.job_detail.duration}</div>
+          <div className="headingValue col-sm-6">
+            {" "}
+            {FilledForm.job_detail.duration}
+          </div>
         </div>
-        {props.type === "INF" ? (
+        {FilledForm.type === "INF" ? (
           <div className="headingdiv">
             <div className="modalHeading col-sm-6">Mode of Internship: </div>
-            <div className="headingValue col-sm-6">{props.job_detail.mode}</div>
+            <div className="headingValue col-sm-6">
+              {FilledForm.job_detail.mode}
+            </div>
           </div>
         ) : (
           <div></div>
@@ -70,59 +78,59 @@ export default function FormModal(props) {
             <div className="headingdiv">
               <div className="modalHeading col-sm-6">Name: </div>
               <div className="headingValue col-sm-6">
-                {props.contact_detail[0].name}
+                {FilledForm.contact_detail[0].name}
               </div>
             </div>
 
             <div className="headingdiv">
               <div className="modalHeading col-sm-6">Designation: </div>
               <div className="headingValue col-sm-6">
-                {props.contact_detail[0].designation}
+                {FilledForm.contact_detail[0].designation}
               </div>
             </div>
 
             <div className="headingdiv">
               <div className="modalHeading col-sm-6">Email: </div>
               <div className="headingValue col-sm-6">
-                {props.contact_detail[0].email}
+                {FilledForm.contact_detail[0].email}
               </div>
             </div>
             <div className="headingdiv">
               <div className="modalHeading col-sm-6">Mobile: </div>
               <div className="headingValue col-sm-6">
-                {props.contact_detail[0].mobile}
+                {FilledForm.contact_detail[0].mobile}
               </div>
             </div>
           </div>
         </div>
-        {props.contact_detail.length === 2 ? (
+        {FilledForm.contact_detail.length === 2 ? (
           <div>
             <div className="lead">Secondary Contact detail</div>
             <div>
               <div className="headingdiv">
                 <div className="modalHeading col-sm-6">Name: </div>
                 <div className="headingValue col-sm-6">
-                  {props.contact_detail[1].name}
+                  {FilledForm.contact_detail[1].name}
                 </div>
               </div>
 
               <div className="headingdiv">
                 <div className="modalHeading col-sm-6">Designation : </div>
                 <div className="headingValue col-sm-6">
-                  {props.contact_detail[1].designation}
+                  {FilledForm.contact_detail[1].designation}
                 </div>
               </div>
 
               <div className="headingdiv">
                 <div className="modalHeading col-sm-6">Email: </div>
                 <div className="headingValue col-sm-6">
-                  {props.contact_detail[1].email}
+                  {FilledForm.contact_detail[1].email}
                 </div>
               </div>
               <div className="headingdiv">
                 <div className="modalHeading col-sm-6">Mobile: </div>
                 <div className="headingValue col-sm-6">
-                  {props.contact_detail[1].mobile}
+                  {FilledForm.contact_detail[1].mobile}
                 </div>
               </div>
             </div>
@@ -137,7 +145,7 @@ export default function FormModal(props) {
         <div>
           <div className="courseType">4-Year B.Tech Programs</div>
           <div>
-            {props.eligible_branch.btech.map((branch) => {
+            {FilledForm.eligible_branch.btech.map((branch) => {
               return <div className="branchName">{branch}</div>;
             })}
           </div>
@@ -147,7 +155,7 @@ export default function FormModal(props) {
             5-Year Dual Degree/ Integrated M.Tech Programs
           </div>
           <div>
-            {props.eligible_branch.dd_mtech.map((branch) => {
+            {FilledForm.eligible_branch.dd_mtech.map((branch) => {
               return <div className="branchName">{branch}</div>;
             })}
           </div>
@@ -155,7 +163,7 @@ export default function FormModal(props) {
         <div>
           <div className="courseType">3-Year MSc. Tech Programs</div>
           <div>
-            {props.eligible_branch.msc3.map((branch) => {
+            {FilledForm.eligible_branch.msc3.map((branch) => {
               return <div className="branchName">{branch}</div>;
             })}
           </div>
@@ -163,7 +171,7 @@ export default function FormModal(props) {
         <div>
           <div className="courseType">2-Year MSc. Tech Programs</div>
           <div>
-            {props.eligible_branch.msc2.map((branch) => {
+            {FilledForm.eligible_branch.msc2.map((branch) => {
               return <div className="branchName">{branch}</div>;
             })}
           </div>
@@ -171,7 +179,7 @@ export default function FormModal(props) {
         <div>
           <div className="courseType">2-Year MBA Programs</div>
           <div>
-            {props.eligible_branch.mba.map((branch) => {
+            {FilledForm.eligible_branch.mba.map((branch) => {
               return <div className="branchName">{branch}</div>;
             })}
           </div>
@@ -179,7 +187,7 @@ export default function FormModal(props) {
         <div>
           <div className="courseType">2-Year M.Tech Programs</div>
           <div>
-            {props.eligible_branch.mtech.map((branch) => {
+            {FilledForm.eligible_branch.mtech.map((branch) => {
               return <div className="branchName">{branch}</div>;
             })}
           </div>
@@ -187,7 +195,7 @@ export default function FormModal(props) {
         <div>
           <div className="courseType">PhD Programs</div>
           <div>
-            {props.eligible_branch.phd.map((branch) => {
+            {FilledForm.eligible_branch.phd.map((branch) => {
               return <div className="branchName">{branch}</div>;
             })}
           </div>
@@ -197,7 +205,7 @@ export default function FormModal(props) {
       <div>
         <div className="categoryName">Skills Requirement:</div>
         <div>
-          {props.skill_based.map((skill) => {
+          {FilledForm.skill_based.map((skill) => {
             return <div className="skill">{skill}</div>;
           })}
         </div>
@@ -209,27 +217,31 @@ export default function FormModal(props) {
           <div className="headingdiv">
             <div className="modalHeading col-sm-6">Resume Shortlisting : </div>
             <div className="headingValue col-sm-6">
-              {props.selection_pr.resume_short_listing}
+              {FilledForm.selection_pr.resume_short_listing}
             </div>
           </div>
 
           <div className="headingdiv">
             <div className="modalHeading col-sm-6">Type of Test : </div>
             <div className="headingValue col-sm-6">
-              {props.selection_pr.type_of_test}
+              {FilledForm.selection_pr.type_of_test}
             </div>
           </div>
 
           <div className="headingdiv">
-            <div className="modalHeading col-sm-6">Other Qualification Rounds : </div>
-            {props.selection_pr.other_round.map((round) => {
+            <div className="modalHeading col-sm-6">
+              Other Qualification Rounds :{" "}
+            </div>
+            {FilledForm.selection_pr.other_round.map((round) => {
               <div className="headingValue col-sm-6">{round}</div>;
             })}
           </div>
           <div className="headingdiv">
-            <div className="modalHeading col-sm-6">Total Number of rounds: </div>
+            <div className="modalHeading col-sm-6">
+              Total Number of rounds:{" "}
+            </div>
             <div className="headingValue col-sm-6">
-              {props.selection_pr.total_rounds}
+              {FilledForm.selection_pr.total_rounds}
             </div>
           </div>
           <div className="headingdiv">
@@ -238,13 +250,15 @@ export default function FormModal(props) {
               sufficient)
             </div>
             <div className="headingValue col-sm-6">
-              {props.selection_pr.no_of_offers}
+              {FilledForm.selection_pr.no_of_offers}
             </div>
           </div>
           <div className="headingdiv">
-            <div className="modalHeading col-sm-6">Eligibility Criteria (if any)</div>
+            <div className="modalHeading col-sm-6">
+              Eligibility Criteria (if any)
+            </div>
             <div className="headingValue col-sm-6">
-              {props.selection_pr.eligible_criteria}
+              {FilledForm.selection_pr.eligible_criteria}
             </div>
           </div>
         </div>
